23b275a639538eb68d3de7755b6378fbda7ff2fd
brianglezn <119687340+brianglezn@users.noreply.github.com>
2023-09-15 21:42:10 +0200

    15/09/23
    Primer commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,10 +4,19 @@ function Header() {
 
     return (
         <header className="header">
+            <div className="header-list">
+                <ul className="menu">
+                    <li><a href="#">Inicio</a></li>
+                    <li><a href="#">Proyecto</a></li>
+                    <li><a href="#">Servicios</a></li>
+                    <li><a href="#">Contacto</a></li>
+                </ul>
+            </div>
             <div className="header-butons">
-                <a className="fa-brands fa-github" href='https://github.com/brianglezn'></a>
-                <a className="fa-brands fa-linkedin" href='https://www.linkedin.com/in/brianglezn/'></a>
-                <a className="fa-brands fa-instagram" href='https://www.instagram.com/brianglezn.es/'></a>
+                <a className="fa-solid fa-house" href='/'></a>
+                <a className="fa-solid fa-folder-tree" href='#projects'></a>
+                <a className="fa-solid fa-tag" href='#services'></a>
+                <a className="fa-solid fa-phone fa-flip-horizontal" href='#contact'></a>
             </div>
         </header>
     )
