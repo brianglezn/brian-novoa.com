6ccef336e80cb170566a3b0ec33007f8ccd0435e
brianglezn <119687340+brianglezn@users.noreply.github.com>
2023-09-22 15:39:48 +0200

    22/09/23
    Img en projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,18 +1,19 @@
-import './Projects.css';
+// import globalthermo from '../assets/projects/mockups/globalthermo.es.jpg';
+/* <img src={globalthermo} /> */
 
-import globalthermo from '../assets/projects/mockups/web-wordpress/globalthermo.es.jpg';
+import './Projects.css';
 
 import projectsData from '../assets/projects/projects.json';
 
 function Projects() {
     const projects = projectsData.map((project) => {
+
         return (
             <div className='projects__project' key={project.id} onClick={() => {
                 const link = project.link;
                 window.open(link, '_blank');
             }}>
-                <img src={globalthermo} />
-                {/* <img src={project.photo} alt={project.name} /> */}
+                <img src={project.photo} alt={project.name} />
                 <p>{project.name}</p>
                 <span>{project.description}</span>
                 <div className='projects__tech'>
