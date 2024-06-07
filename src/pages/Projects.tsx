077d380e5d3e84a36aec699bd8d4b6eb4ff5597f
Brian González Novoa <119687340+brianglezn@users.noreply.github.com>
2024-06-07 18:58:30 +0200

    Cambios css Projects

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -11,30 +11,31 @@ export default function Projects() {
         <>
             <Header />
             <section className='projects'>
-                <h2>Projects</h2>
-                <p>These are the projects in which I have worked, I started making wordpress websites for a few companies and now I took the step to make code projects especially with MERN stack: Mongo, Express, React and Node.</p>
+                <div>
+                    <h2>Projects</h2>
+                    <p>These are the projects in which I have worked, I started making wordpress websites for a few companies and now I took the step to make code projects especially with MERN stack: Mongo, Express, React and Node.</p>
 
-                <section className="projectsList">
-                    <ProjectItem
-                        imgSrc={logoPL}
-                        title="Profit & Lost"
-                        description="App for personal finance management"
-                        href="/projects/profit-and-lost"
-                    />
-                    <ProjectItem
-                        imgSrc={logoWordpress}
-                        title="Wordpress Webs"
-                        description="Some of the websites I have done in Wordpress"
-                        href="/projects/wordpress-webs"
-                    />
-                    <ProjectItem
-                        imgSrc={logoElearning}
-                        title="eLearning Platform"
-                        description="App in progress for an e-learning platform"
-                        href="/projects/elearning-platform"
-                    />
-                </section>
-
+                    <section className="projectsList">
+                        <ProjectItem
+                            imgSrc={logoPL}
+                            title="Profit & Lost"
+                            description="App for personal finance management"
+                            href="/projects/profit-and-lost"
+                        />
+                        <ProjectItem
+                            imgSrc={logoWordpress}
+                            title="Wordpress Webs"
+                            description="Some of the websites I have done in Wordpress"
+                            href="/projects/wordpress-webs"
+                        />
+                        <ProjectItem
+                            imgSrc={logoElearning}
+                            title="eLearning Platform"
+                            description="App in progress for an e-learning platform"
+                            href="/projects/elearning-platform"
+                        />
+                    </section>
+                </div>
             </section>
             <Footer />
         </>
