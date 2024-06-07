a704d375a2bac171b889f99d33487ef329b337ad
Brian González Novoa <119687340+brianglezn@users.noreply.github.com>
2024-06-07 18:23:06 +0200

    Sección About completada
    +Se agregan cambios de css a todas las páginas

--- src/helpers/utils.ts
+++ src/helpers/utils.ts
@@ -1,8 +1,13 @@
 export function calculateDuration(startDate: string, endDate: string): string {
-    const start = new Date(startDate);
-    const end = endDate.toLowerCase() === 'actualidad' ? new Date() : new Date(endDate);
-    const diffInMonths = (end.getFullYear() - start.getFullYear()) * 12 + (end.getMonth() - start.getMonth());
-    const years = Math.floor(diffInMonths / 12);
-    const months = diffInMonths % 12;
-    return `${years > 0 ? `${years} ${years > 1 ? 'años' : 'año'}` : ''} ${months > 0 ? `${months} ${months > 1 ? 'meses' : 'mes'}` : ''}`.trim();
+  const start = new Date(startDate);
+  const end =
+    endDate.toLowerCase() === "actualidad" ? new Date() : new Date(endDate);
+  const diffInMonths =
+    (end.getFullYear() - start.getFullYear()) * 12 +
+    (end.getMonth() - start.getMonth());
+  const years = Math.floor(diffInMonths / 12);
+  const months = diffInMonths % 12;
+  return `${years > 0 ? `${years} ${years > 1 ? "años" : "año"}` : ""} ${
+    months > 0 ? `${months} ${months > 1 ? "meses" : "mes"}` : ""
+  }`.trim();
 }
