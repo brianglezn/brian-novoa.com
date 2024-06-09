9777dbd56aabf82fde09754c6f74c387ded1170f
brianglezn <119687340+brianglezn@users.noreply.github.com>
2024-06-09 21:28:21 +0200

    reemplazo de imagen PL

--- src/pages/ProjectPage.tsx
+++ src/pages/ProjectPage.tsx
@@ -4,7 +4,7 @@ import { useTranslation } from 'react-i18next';
 import './ProjectPage.scss';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
-import MainProfitLost from '../assets/projects/PL/profit-lost.com.png';
+import MainProfitLost from '../assets/projects/PL/profit-lost.com.jpg';
 import MainWP from '../assets/projects/WP/labarveria.com.png';
 import MainELearning from '../assets/projects/eLearning/elearning.com.png';
 import LogoProfitLost from '../assets/projects/PL/logoPL.svg';
